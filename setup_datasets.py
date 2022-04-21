72d31e56e168b8ab03348810d4c5bac0f8a90a7a
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2022-04-21 14:57:01 +0200

    Fixed typo

--- setup_datasets.py
+++ setup_datasets.py
@@ -242,4 +242,4 @@ if __name__ == "__main__":
 
     if args.download:
         download_datasets(args.data_path, args.datasets)
-    generate_metadata(args.data_path)
+    generate_metadata(args.data_path, args.datasets)
